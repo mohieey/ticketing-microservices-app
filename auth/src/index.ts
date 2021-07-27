ffd83affc926491c72139f333c9967bf1dafdf6e
Mohamed Mohiey <mohhiey@gmail.com>
2021-07-27 20:52:49 +0200

    added auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+//Routes
+import {currentUserRouter} from './routes/currentUser';
+import {signupRouter} from './routes/signup';
+import {signinRouter} from './routes/signin';
+import {signoutRouter} from './routes/signout';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser',(req, res) => {
-    res.send('fidfjhdifjdifj')
-})
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 app.listen(3000,()=>{
     console.log('listening on 3000');
