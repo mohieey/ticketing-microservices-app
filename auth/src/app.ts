c862df420826d93b16ba8e491df491263f15bc04
Mohamed Mohiey <mohhiey@gmail.com>
2021-08-16 17:34:04 +0200

    refactored some code

---
+++ auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
+//Routes
+import { currentUserRouter } from "./routes/currentUser";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { NotFoundError } from "./errors/notFoundError";
+//Middlewares
+import { errorHandler } from "./middlewares/errorHandler";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
+
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,5 @@
-import express from "express";
-import { json } from "body-parser";
-import "express-async-errors";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-//Routes
-import { currentUserRouter } from "./routes/currentUser";
-import { signupRouter } from "./routes/signup";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { NotFoundError } from "./errors/notFoundError";
-//Middlewares
-import { errorHandler } from "./middlewares/errorHandler";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(cookieSession({ signed: false, secure: true }));
-
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
