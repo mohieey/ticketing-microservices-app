be187e762d4e8ff95258eb98bc718d918aa29a0f
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-10 08:05:08 +0200

    started building orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { NotFoundError, errorHandler, currentUser } from "@ticmoh/common";
+
+//Routes
+import { newTicketRouter } from "./routes/newTicket";
+import { getTicketRouter } from "./routes/getTicket";
+import { getAllTicketsRouter } from "./routes/getAllTickets";
+import { updateTicketRouter } from "./routes/updateTicket";
+// import { signoutRouter } from "./routes/signout";
+//Middlewares
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+app.use(currentUser);
+
+app.use(newTicketRouter);
+app.use(getTicketRouter);
+app.use(getAllTicketsRouter);
+app.use(updateTicketRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
