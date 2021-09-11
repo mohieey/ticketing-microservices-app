49b4039f033e3ef421b06d55d2d8244492a0a647
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-11 23:53:02 +0200

    implemented getting single ticket for the logged in user endpoint

--- orders/src/routes/getOrder.ts
+++ orders/src/routes/getOrder.ts
@@ -1,13 +1,23 @@
 import express, { Response, Request } from "express";
+import { requireAuth, NotFoundError, NotAuthorizedError } from "@ticmoh/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
 router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  //   if (!ticketInDB) {
-  //     throw new NotFoundError();
-  //   }
+  const { orderId } = req.params;
 
-  return res.send({});
+  const orderInDb = await Order.findById(orderId).populate("ticket");
+
+  if (!orderInDb) {
+    throw new NotFoundError();
+  }
+
+  if (orderInDb.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  return res.send(orderInDb);
 });
 
 export { router as getOrderRouter };
