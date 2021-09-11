cb05c402f699c25100b0cc78e34fde0499b4c0e9
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-11 08:48:41 +0200

    implemented some tests around new order endpoint in orders service

---
+++ orders/src/routes/__tests__/newOrder.test.ts
@@ -0,0 +1,56 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+import { createCookie } from "../../test/setup";
+
+it("Returns an error if the ticket does not exist", async () => {
+  const ticketId = mongoose.Types.ObjectId();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", createCookie())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it("Returns an error if the ticket is already reserved", async () => {
+  const ticket = Ticket.build({ title: "Test Ticket", price: 34 });
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: "rtopygijg",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", createCookie())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it("Reserves a ticket", async () => {
+  const ticket = Ticket.build({ title: "Test Ticket", price: 34 });
+  await ticket.save();
+
+  // const order = Order.build({
+  //   ticket,
+  //   userId: "rtopygijg",
+  //   status: OrderStatus.Created,
+  //   expiresAt: new Date(),
+  // });
+  // await order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", createCookie())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
+
+it.todo("Emits an event when an order is created");
