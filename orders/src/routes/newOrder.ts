c50f84efcc9ef1ea4addd2992ebb5600331e62a6
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-11 06:51:33 +0200

    implemented order and ticket models in orders service

--- orders/src/routes/newOrder.ts
+++ orders/src/routes/newOrder.ts
@@ -1,13 +1,28 @@
 import express, { Response, Request } from "express";
+import { requireAuth, validateRequest } from "@ticmoh/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  //   if (!ticketInDB) {
-  //     throw new NotFoundError();
-  //   }
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((id) => mongoose.Types.ObjectId.isValid(id))
+      .withMessage("Ticket id must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    //   if (!ticketInDB) {
+    //     throw new NotFoundError();
+    //   }
 
-  return res.send({});
-});
+    return res.send({});
+  }
+);
 
 export { router as newOrderRouter };
