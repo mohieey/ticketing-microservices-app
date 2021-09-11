76f15d824e005c606f15410b449e1cade1506463
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-11 08:58:44 +0200

    implemented get all orders for logged in user endpoint in orders service

--- orders/src/routes/getAllOrders.ts
+++ orders/src/routes/getAllOrders.ts
@@ -1,13 +1,16 @@
 import express, { Response, Request } from "express";
+import { requireAuth } from "@ticmoh/common";
+
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/tickets", async (req: Request, res: Response) => {
-  //   if (!ticketInDB) {
-  //     throw new NotFoundError();
-  //   }
+router.get("/api/tickets", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({ userId: req.currentUse!.id }).populate(
+    "ticket"
+  );
 
-  return res.send({});
+  return res.send(orders);
 });
 
 export { router as getAllOrdersRouter };
