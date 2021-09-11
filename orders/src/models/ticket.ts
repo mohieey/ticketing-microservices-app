f7f9e4c9a7caca4b53240f24f24d3d948aaf9033
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-11 08:00:59 +0200

    implemented new order endpoint in orders service

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+import { Order, OrderStatus } from "./order";
+
 interface TicketAttrs {
   title: string;
   price: number;
@@ -8,6 +10,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -29,8 +32,28 @@ const ticketSchema = new mongoose.Schema(
   }
 );
 
+//Add a method to Model
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
+};
+
+//Add a method to Document
+ticketSchema.methods.isReserved = async function () {
+  //this === the ticket doc that we called isReserved on
+
+  const validOrderWithTheSameTicket = await Order.findOne({
+    //@ts-ignore
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return !!validOrderWithTheSameTicket;
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
--- orders/src/routes/newOrder.ts
+++ orders/src/routes/newOrder.ts
@@ -1,9 +1,20 @@
 import express, { Response, Request } from "express";
-import { requireAuth, validateRequest } from "@ticmoh/common";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  BadRequestError,
+  OrderStatus,
+} from "@ticmoh/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
 
+import { Order } from "../models/order";
+import { Ticket } from "../models/ticket";
+
 const router = express.Router();
+
+const Expiration_WINDOW_IN_SECONDS = 15 * 24;
 
 router.post(
   "/api/orders",
@@ -17,11 +28,31 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    //   if (!ticketInDB) {
-    //     throw new NotFoundError();
-    //   }
+    const { ticketId } = req.body;
 
-    return res.send({});
+    const ticketInDb = await Ticket.findById(ticketId);
+    if (!ticketInDb) {
+      throw new NotFoundError();
+    }
+
+    const isReserved = await ticketInDb.isReserved();
+
+    if (isReserved) {
+      throw new BadRequestError("This ticket is already reserved");
+    }
+
+    const expiresAt = new Date();
+    expiresAt.setSeconds(expiresAt.getSeconds() + Expiration_WINDOW_IN_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiresAt,
+      ticket: ticketInDb,
+    });
+    await order.save();
+
+    return res.status(201).send(order);
   }
 );
 
