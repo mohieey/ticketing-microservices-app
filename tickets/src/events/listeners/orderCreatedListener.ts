2e8857e11cdf3c943318d23735b6f757560f6bf2
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-22 15:01:26 +0200

    implemented orderCreatedListener in tickets service

--- tickets/src/events/listeners/orderCreatedListener.ts
+++ tickets/src/events/listeners/orderCreatedListener.ts
@@ -1,12 +1,27 @@
 import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@ticmoh/common";
 import { queueGroupName } from "./queueGroupName";
+import { Ticket } from "../../models/ticket";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    throw new Error("Method not implemented.");
+    const ticketInDB = await Ticket.findById(data.ticket.id);
+
+    try {
+      if (!ticketInDB) {
+        throw new Error("Ticket not found");
+      }
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+
+    ticketInDB.set({ orderId: data.id });
+    await ticketInDB.save();
+
+    msg.ack();
   }
 }
