ee68eaffefc0de39937938fe433e66076e33a46b
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-22 16:02:52 +0200

    modified orderCreatedListener in tickets service

--- tickets/src/events/listeners/orderCreatedListener.ts
+++ tickets/src/events/listeners/orderCreatedListener.ts
@@ -2,6 +2,7 @@ import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@ticmoh/common";
 import { queueGroupName } from "./queueGroupName";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "./../publishers/ticketUpdatedPublisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -21,6 +22,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     ticketInDB.set({ orderId: data.id });
     await ticketInDB.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticketInDB.id,
+      price: ticketInDB.price,
+      title: ticketInDB.title,
+      version: ticketInDB.version,
+      userId: ticketInDB.userId,
+      orderId: ticketInDB.orderId,
+    });
 
     msg.ack();
   }
