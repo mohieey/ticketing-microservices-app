0ef11f45b5ebdf006e960708969809ef2fa67728
Mohamed Mohiey <mohhiey@gmail.com>
2021-09-29 10:29:59 +0200

    orders service recieves the expiration event and cancelles the order and publishes a cancelling event

---
+++ orders/src/events/listeneres/expirationCompleteListener.ts
@@ -0,0 +1,39 @@
+import { Message } from "node-nats-streaming";
+import {
+  Subjects,
+  Listener,
+  ExpirationCompleteEvent,
+  OrderStatus,
+} from "@ticmoh/common";
+
+import { queueGroupName } from "./queueGroupName";
+import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "./../publishers/orderCancelledPublisher";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const existingOrder = await Order.findById(data.orderId).populate("ticket");
+
+    try {
+      if (!existingOrder) {
+        throw new Error(`Order No. ${data.orderId} Not Found`);
+      }
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+
+    existingOrder.set({ orderStatus: OrderStatus.Cancelled });
+    await existingOrder.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: existingOrder.id,
+      version: existingOrder.version,
+      ticket: { id: existingOrder.ticket.id },
+    });
+
+    msg.ack();
+  }
+}
