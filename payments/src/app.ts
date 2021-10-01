f97bc3beb38a3ea3ddbc55ab9d4347c094262957
Mohamed Mohiey <mohhiey@gmail.com>
2021-10-01 10:27:09 +0200

    instantiated payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { NotFoundError, errorHandler, currentUser } from "@ticmoh/common";
+
+//Routes
+
+// import { signoutRouter } from "./routes/signout";
+//Middlewares
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+app.use(currentUser);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,61 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
+
+let mongo: any;
+
+jest.mock("../natsWrapper");
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "fdisjfuidshfui";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+export const createCookie = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return `express:sess=${base64}`;
+};
+
+export const createTicket = (cookie?: any) => {
+  return request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie || createCookie())
+    .send({ title: "ticketTitle", price: 34 })
+    .expect(201);
+};
