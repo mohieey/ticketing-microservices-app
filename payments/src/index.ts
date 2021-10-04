3bbdd23bbdf85d2ba381b290bef7ccc4d70e09a1
Mohamed Mohiey <mohhiey@gmail.com>
2021-10-04 11:09:31 +0200

    orders service now listens to created payment events

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 import { natsWrapper } from "./natsWrapper";
 import { OrderCreatedListener } from "./events/listeners/orderCreatedListener";
 import { OrderCancelledListener } from "./events/listeners/orderCancelledListener";
+import { PaymentCreatedListener } from "./../../orders/src/events/listeneres/paymentCreatedListener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -42,6 +43,7 @@ const start = async () => {
 
     new OrderCreatedListener(natsWrapper.client).listen();
     new OrderCancelledListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
